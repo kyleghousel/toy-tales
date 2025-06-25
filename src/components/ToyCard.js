92129ac94a6f6e2eac304d2b7b760eb7a314b8de
Kyle Housel <kylegranthousel@gmail.com>
2025-06-25 08:31:34 -0400

    Complete first deliverable

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-function ToyCard() {
+const ToyCard = ({ id, image, likes, name }) => {
   return (
-    <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+    <div className="card" id={id}>
+      <h2>{name && name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={image && image}
+        alt={name && name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
+      <p>{likes && likes} Likes </p>
       <button className="like-btn">Like {"<3"}</button>
       <button className="del-btn">Donate to GoodWill</button>
     </div>
