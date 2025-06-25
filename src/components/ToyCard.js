e76382986caea6fd05723d600838fa7f5a29f4e7
Kyle Housel <kylegranthousel@gmail.com>
2025-06-25 09:17:20 -0400

    Delete request successful

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { API } from './App'
 
 const ToyCard = ({ id, image, likes, name }) => {
+
+  const handleDelete = () => {
+    fetch(API + id, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error("Delete failed")
+        console.log('Post deleted successfully')
+        // Optional: update local state here
+      })
+      .catch((err) => {
+        console.error('Error deleting post:', err)
+      });
+
+  }
+
   return (
     <div className="card" id={id}>
       <h2>{name && name}</h2>
@@ -11,7 +28,7 @@ const ToyCard = ({ id, image, likes, name }) => {
       />
       <p>{likes && likes} Likes </p>
       <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <button className="del-btn" onClick={handleDelete}>Donate to GoodWill</button>
     </div>
   );
 }
